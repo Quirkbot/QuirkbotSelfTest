3310618867441ed44cfdb2edcba00909d9dc2407
Paulo Barcelos <paulo@azucrina.org>
2016-06-12 09:08:40 +0200

    Updated base.ino and QuirkbotArduinoHardware

--- base.ino
+++ base.ino
@@ -2,24 +2,24 @@
 #include <avr/wdt.h>
 
 // Defines ---------------------------------------------------------------------
-#define LE	8	// Left Eye		 	(PB4)	ADC11
-#define RE	A5	// Right Eye		(PF0)	ADC0
-#define LLF	9	// Left Leg Front	(PB5)	ADC12	PWM (16BIT)
-#define RLF	11	// Right Leg Front	(PB7)			PWM (8/16BIT)
-#define RAF	5	// Right Arm Front	(PC6)			PWM (HS)
-#define HF	13	// Horn Front		(PC7)			PWM (10BIT)
-#define LAF	10	// Left Arm Front	(PB6)	ADC13	PWM (16BIT)
-#define LLB	A0	// Left Leg Back	(PF7)	ADC7
-#define RLB	A4	// Right Leg Back	(PF1)	ADC1
-#define RAB	A3	// Right Arm Back	(PF4)	ADC4
-#define HB	A2	// Horn Back		(PF5)	ADC5
-#define LAB	A1	// Left Arm Back	(PF6)	ADC6
-#define BP1	A7	// Back Pack 1		(PD7)	ADC10	PWM (HS)	// or digital 6
-#define BP2	A11	// Back Pack 2		(PD6)	ADC9                    // or digital 12
-#define BP3	0	// Back Pack 3		(PD2)	RXD1
-#define BP4	2	// Back Pack 4		(PD1)	SDA
-#define BP5	3	// Back Pack 5		(PD0)	SCL
-#define BP6	1 	// Back Pack 6		(PD3)	TXD1
+#define LE  8 // Left Eye     (PB4) ADC11
+#define RE  A5  // Right Eye    (PF0) ADC0
+#define LLF 9 // Left Leg Front (PB5) ADC12 PWM (16BIT)
+#define RLF 11  // Right Leg Front  (PB7)     PWM (8/16BIT)
+#define RAF 5 // Right Arm Front  (PC6)     PWM (HS)
+#define HF  13  // Horn Front   (PC7)     PWM (10BIT)
+#define LAF 10  // Left Arm Front (PB6) ADC13 PWM (16BIT)
+#define LLB A0  // Left Leg Back  (PF7) ADC7
+#define RLB A4  // Right Leg Back (PF1) ADC1
+#define RAB A3  // Right Arm Back (PF4) ADC4
+#define HB  A2  // Horn Back    (PF5) ADC5
+#define LAB A1  // Left Arm Back  (PF6) ADC6
+#define BP1 A7  // Back Pack 1    (PD7) ADC10 PWM (HS)  // or digital 6
+#define BP2 A11 // Back Pack 2    (PD6) ADC9                    // or digital 12
+#define BP3 0 // Back Pack 3    (PD2) RXD1
+#define BP4 2 // Back Pack 4    (PD1) SDA
+#define BP5 3 // Back Pack 5    (PD0) SCL
+#define BP6 1   // Back Pack 6    (PD3) TXD1
 #define PULL_UP_PIN 4
 
 // Multiplex -------------------------------------------------------------------
@@ -39,14 +39,14 @@ void _mux(int s0, int s1, int s2){
 }
 void mux(int pin){
 	switch (pin) {
-		case 0:	_mux(MUX_0); break;
-		case 1:	_mux(MUX_1); break;
-		case 2:	_mux(MUX_2); break;
-		case 3:	_mux(MUX_3); break;
-		case 4:	_mux(MUX_4); break;
-		case 5:	_mux(MUX_5); break;
-		case 6:	_mux(MUX_6); break;
-		case 7:	_mux(MUX_7); break;
+		case 0: _mux(MUX_0); break;
+		case 1: _mux(MUX_1); break;
+		case 2: _mux(MUX_2); break;
+		case 3: _mux(MUX_3); break;
+		case 4: _mux(MUX_4); break;
+		case 5: _mux(MUX_5); break;
+		case 6: _mux(MUX_6); break;
+		case 7: _mux(MUX_7); break;
 	}
 }
 // Entry -----------------------------------------------------------------------
@@ -240,45 +240,30 @@ void setup() {
 
 	// Program
 	horn.place = H;
-	horn.sensitivity = 0;
-	horn.min = 0;
-	horn.max = 1;
 
 	amplify1.in.connect(wave1.out);
 	amplify1.inMin = 0.25;
 	amplify1.inMax = 0.75;
-	amplify1.outMin = 0;
-	amplify1.outMax = 1;
 
 	amplify2.in.connect(wave2.out);
 	amplify2.inMin = 0.25;
 	amplify2.inMax = 0.75;
-	amplify2.outMin = 0;
-	amplify2.outMax = 1;
 
 	converter1.in.connect(horn.out);
-	converter1.inMin = 0;
-	converter1.inMax = 1;
 	converter1.outMin = 0.8;
 	converter1.outMax = 0.4;
 
 	converter2.in.connect(horn.out);
-	converter2.inMin = 0;
-	converter2.inMax = 1;
 	converter2.outMin = 0.4;
 	converter2.outMax = 0.8;
 
 	wave1.length.connect(converter1.out);
-	wave1.type = WAVE_SINE;
 	wave1.min = 0.25;
 	wave1.max = 0.75;
-	wave1.offset = 0;
 
 	wave2.length.connect(converter2.out);
-	wave2.type = WAVE_SINE;
 	wave2.min = 0.25;
 	wave2.max = 0.75;
-	wave2.offset = 0;
 
 	leftEye.light.connect(amplify1.out);
 	leftEye.place = LE;
